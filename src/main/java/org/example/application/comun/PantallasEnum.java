c630937c3d48bc5a8faee17fa3ac0b0524b9dcda
irpoveda <irpoveda@minsait.com>
2022-11-28 17:33:55 +0100

    Creacion de menu y cabecera con ejemplos de import, globalcommands y paso de variables del zul a vm

---
+++ src/main/java/org/example/application/comun/PantallasEnum.java
@@ -0,0 +1,18 @@
+package org.example.application.comun;
+
+import lombok.Getter;
+
+@Getter
+public enum PantallasEnum {
+
+  CARLIST("/vm/carlist/carlist.zul"), //
+  EXTENDS("/vm/initoverride/base.zul"), //
+  BASE("/vm/myviewmodel/myviewmodel.zul");
+
+  String path;
+
+  PantallasEnum(String path) {
+    this.path = path;
+  }
+
+}
