ed279ecfbbe6a3caf4e2b35cfb418b01ea19c914
irpoveda <irpoveda@minsait.com>
2022-12-11 23:25:29 +0100

    limpieza de la rama para la prueba

--- src/main/java/org/example/application/vm/IndexVM.java
+++ src/main/java/org/example/application/vm/IndexVM.java
@@ -1,10 +1,8 @@
 package org.example.application.vm;
 
-import org.example.application.comun.PantallasEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.zkoss.bind.annotation.*;
-import org.zkoss.zk.ui.event.Event;
+import org.zkoss.bind.annotation.Init;
 
 import lombok.Getter;
 
@@ -13,19 +11,9 @@ public class IndexVM {
 
   private static final Logger LOG = LoggerFactory.getLogger(IndexVM.class);
 
-  private String pathCentro;
-
   @Init
   public void initIndexVM() {
     LOG.info("IndexVM.initIndexVM()");
-  }
-
-  @Command("navegarA")
-  @GlobalCommand("navegarA")
-  @NotifyChange("pathCentro")
-  public void navegarA(Event e, @BindingParam("path") PantallasEnum ruta) {
-    LOG.info("command");
-    pathCentro = ruta.getPath();
   }
 
 }
